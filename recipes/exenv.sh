acc6f10ad3deb63cfb57ecbf81e04685882a4d5b
Zahidul Haque <iisowrovii@gmail.com>
2017-12-01 11:02:08 +0600

    install script for exenv

---
+++ recipes/exenv.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if [ -d ~/.exenv ]; then
+  echo 'exenv already installed. Skipping...'
+else
+  sudo apt-get install -y erlang erlang-doc
+  git clone https://github.com/mururu/exenv.git ~/.exenv
+  mkdir -p ~/.exenv/plugins
+  git clone https://github.com/mururu/elixir-build.git ~/.exenv/plugins/elixir-build
+fi
