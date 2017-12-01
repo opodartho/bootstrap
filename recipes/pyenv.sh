37b03a638773817f2e7bb49002d32c64e7f737a1
Zahidul Haque <iisowrovii@gmail.com>
2017-12-01 10:25:49 +0600

    install script for pyenv

---
+++ recipes/pyenv.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+if [ -d ~/.pyenv ]; then
+  echo '########## pyenv already installed, skipping.'
+else
+  echo "########## Installing pyenv ..."
+  sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev git
+  sudo apt-get install -y build-essential checkinstall
+  sudo apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+  git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+  mkdir -p ~/.pyenv/plugins
+  pushd ~/.pyenv/plugins
+  git clone https://github.com/pyenv/pyenv-ccache.git
+  git clone https://github.com/pyenv/pyenv-version-ext.git
+  popd
+fi
--- recipes/rbenv.sh
+++ recipes/rbenv.sh
@@ -4,9 +4,6 @@ if [ -d ~/.rbenv ]; then
 else
   sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev git
   git clone git://github.com/rbenv/rbenv.git ~/.rbenv
-  pushd ~/.rbenv
-  pushd src/configure && make -C src && popd
-  popd
   mkdir -p ~/.rbenv/plugins
   pushd ~/.rbenv/plugins
   git clone git://github.com/rbenv/ruby-build.git
