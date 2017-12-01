c6b1291345c915bd5f739a5c8e6a5fa8ccdd151d
Zahidul Haque <iisowrovii@gmail.com>
2017-12-01 13:06:03 +0600

    update to virtualbox-5.2

--- recipes/virtualbox-5.sh
+++ recipes/virtualbox-5.sh
@@ -7,8 +7,8 @@ if [[ -f /etc/apt/sources.list.d/virtualbox.list ]]; then
   echo "PPA already added. Skipping ..."
 else
   echo "deb http://download.virtualbox.org/virtualbox/debian ${release_name} contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-  wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+  wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
   sudo apt-get update -y
 fi
 
-sudo apt-get install virtualbox-5.1 -y
+sudo apt-get install virtualbox-5.2 -y
