2aa61c51d96fd96e396ee5c1298397b716bd6242
Zahidul Haque <iisowrovii@gmail.com>
2017-12-08 23:24:33 +0600

    remove pyenv-ccache & pyenv-version-ext plugins

--- recipes/pyenv.sh
+++ recipes/pyenv.sh
@@ -6,9 +6,4 @@ else
   sudo apt-get install -y build-essential checkinstall
   sudo apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
   git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-  mkdir -p ~/.pyenv/plugins
-  pushd ~/.pyenv/plugins
-  git clone https://github.com/pyenv/pyenv-ccache.git
-  git clone https://github.com/pyenv/pyenv-version-ext.git
-  popd
 fi
